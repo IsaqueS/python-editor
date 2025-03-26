b081ae2e36113e46cf256c4cdf25305eb188da6b
Isaque Sebold de Almeida <38296655+IsaqueS@users.noreply.github.com>
2025-03-25 21:16:52 -0300

    Debugger setting added

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ app = Flask(__name__)
 def hello_world() -> str:
     return render_template("index.html")
 
-app.run(host="0.0.0.0", port=80)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
